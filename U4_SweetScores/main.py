2c9389805daa718ac194924206ed54c5122ef382
Mishu Bell <mbosb@pm.me>
2025-06-13 18:23:29 +0200

    Add hand display and round progress features
    - Implement display_hand to show cards in hand with indices
    - Add display_hand_score to show the hand's total value
    - Add rounds_progress to indicate current round with a progress bar

--- U4_SweetScores/main.py
+++ U4_SweetScores/main.py
@@ -1,46 +1,65 @@
 import random
 #TODO: Sanitize Inputs
 
-hand = []
-npc_hand = []
 deck = ['kiwi', 'apple', 'apple', 'orange', 'banana','apple', 'fruit fly', 'poison apple', 'banana', 'cherry']
-user_points = 0
-npc_points = 0
 
 def main():
-    
-    hand = build_hand()
-    hand = discard_cards(hand)
-    print(evaluate_hand(hand))
-    pass
+    rounds = 0
+    max_rounds = 5
 
+    print(welcome_user())
+    while rounds < max_rounds:
+        rounds_progress(rounds, max_rounds)
 
+        hand = build_hand()
+        display_hand_score(hand)
+        hand = discard_cards(hand, True)
+        display_hand_score(hand)
+        user_score = evaluate_hand(hand)
+        display_hand_score(hand)
+        # User Score
+
+        npc_hand = build_hand()
+        npc_hand = discard_cards(npc_hand, False)
+        npc_score = evaluate_hand(npc_hand)
+        # NPC Score
+
+        if user_score > npc_score:
+            print("You've won the round!")
+        else:
+            print("NPC won the round.")
+
+        rounds += 1
 
 # Helpers
 def build_hand():
+    hand = []
     # Fill hand with 6 cards
     for i in range(0, 6):
         rand_pick = random.randint(1,6)
         hand.append(deck[rand_pick])
     return hand
 
-def discard_cards(hand):
-
-    # Show hand
-    print(hand) 
-
-    while len(hand) >= 5:
-        print("Discard a card by entering a valid number! \n")
-        print("Type a num:")
-        index = int(input())
-        hand.pop(index - 1)
+def discard_cards(hand, player):
+    
+    # print(hand) 
+    if player == True:
+        while len(hand) >= 5:
+            # print discard instruction
+            display_hand(hand)
+            index = int(input())
+            hand.pop(index - 1)
+    else:
+        while len(hand) >= 5:
+            index = random.randint(1, len(hand))
+            hand.pop(index - 1)
+            # DEBUG ONLY: Print NPC hand
     return hand
 
 def evaluate_hand(hand):
     score = 0
     for card in hand:
         score += evaluate_card(card)
-        print(card)
     return score
 
 def evaluate_card(card):
@@ -58,5 +77,25 @@ def evaluate_card(card):
     elif card == "cherry":
         score +=1
     return score
+
+def welcome_user():
+    welcome_text = '''
+Welcome! In this game, you will be given 6 cards to your hand,
+of which you choose two to be discarded. Each card has different effects that add or subtract to your score.
+But watch out! Some cards have special effects!
+'''
+    return welcome_text
+
+def rounds_progress(round, max_rounds):
+    filled = '#' * round
+    empty = '-' * (max_rounds - round)
+    print( f"Round: [{filled}{empty}]")  
+
+def display_hand(hand):
+    for i in range(len(hand)):
+        print(str(i + 1) + ": " + hand[i])
+
+def display_hand_score(hand):
+    print("Your hand is valued at: " + str(evaluate_hand(hand)))
 
 main()
